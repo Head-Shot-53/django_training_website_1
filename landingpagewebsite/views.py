6f02c1ec00e6bc24c177c8874d39a5f40e95ee8c
Head-Shot-53 <denchikmashtack@gmail.com>
2025-01-12 16:54:29 +0200

    закоментував всі функції бо вони поки не потрібні

--- landingpagewebsite/views.py
+++ landingpagewebsite/views.py
@@ -2,13 +2,13 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
-def first_page(request):
-    return HttpResponse('Hello world')
+# def first_page(request):
+#     return HttpResponse('Hello world')
 
-def second_page(request):
-    a = '<h1>Hello world<h1>'
-    text = 'Hello'
-    return render(request, './index.html',{
-        'a':a,
-        'text': text
-    })
+# def second_page(request):
+#     a = '<h1>Hello world<h1>'
+#     text = 'Hello'
+#     return render(request, './index.html',{
+#         'a':a,
+#         'text': text
+#     })
